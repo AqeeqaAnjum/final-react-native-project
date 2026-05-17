16c8726bbe7b82a5186a7b067e1f0fbb3148045f
AqeeqaAnjum <muqadusanjum2002@gmail.com>
2026-05-17 17:26:31 +0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,1 +1,15 @@
 
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function Header({ onOpenSettings }) {
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <Text>OSLER AI</Text>
+
+      <TouchableOpacity onPress={onOpenSettings}>
+        <Text>⚙️</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
