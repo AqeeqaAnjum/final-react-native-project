b2cedfdc99e8bddf4a3e220c43ff664f524f56b0
AqeeqaAnjum <muqadusanjum2002@gmail.com>
2026-05-17 17:32:22 +0500

    Update notifications.js

--- services/notifications.js
+++ services/notifications.js
@@ -1,1 +1,12 @@
 
+import * as Notifications from "expo-notifications";
+
+export const sendNotification = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "OSLER AI Alert",
+      body: "New update available!",
+    },
+    trigger: null,
+  });
+};
